ba339a255103829cfd8f8c488d5b23707a4ede11
Sander Voerman <sander.voerman@kabisa.nl>
2024-11-09 14:44:37 +0100

    Correct path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
+  basePath: "/github-star-counter",
+  output: "export",
   reactStrictMode: true,
 };
 
